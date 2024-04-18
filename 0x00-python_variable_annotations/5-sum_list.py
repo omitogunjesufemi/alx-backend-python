03513606aea627b8c48fa89751b44d9bb114cf61
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-18 23:55:24 +0100

    Fixed pycodestyle on file 5-sum_list.py and 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,8 @@ and returns their sum as a float
 """
 from typing import List
 
-def sum_list(input_list: List[float] ) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """
     A type-annotated function which takes a list of floats as arguments
     and returns their sum as a float
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@ floats as arguments and returns their sum as a float
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]] ) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     A type-annotated function which takes a list of integers and
     floats as arguments and returns their sum as a float
