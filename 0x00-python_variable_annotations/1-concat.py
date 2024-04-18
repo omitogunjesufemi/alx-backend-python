4723da2e7ee845fa7ddf2dfc501bcf4097b2562a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-18 21:24:59 +0100

    A type-annoted function that takes str args and returns str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function that takes strings arguments and
+returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A type-annotated function that takes strings arguments and
+    returns a concatenated string
+    """
+    return (str1 + str2)
