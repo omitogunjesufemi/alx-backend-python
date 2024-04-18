b35a6cbfba31ba5d0c1a9267251e60a8315de585
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-18 23:53:23 +0100

    Type-annotated function that takes a list of floats and ints as arg and returns their float sum

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function which takes a list of integers and
+floats as arguments and returns their sum as a float
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]] ) -> float:
+    """
+    A type-annotated function which takes a list of integers and
+    floats as arguments and returns their sum as a float
+    """
+    total: float = 0.0
+    for element in mxd_lst:
+        total += element
+    return total
