7a482ca6efc1a681d30cc784009ce9d1664e1687
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-19 01:15:37 +0100

    Fixed pycodestyle: 101-safely_get_value.py:10:1: E302 expected 2 blank lines, found 1

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,6 +7,7 @@ from typing import TypeVar, Optional, Any, Mapping, Union
 
 T = TypeVar("T")
 
+
 def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
     """
