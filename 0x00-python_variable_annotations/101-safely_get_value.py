96e7b0334f6a54a6b068a8c86bec28a8dc3e3c01
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-19 01:09:09 +0100

    Duck typing annotation using Mapping, Optional, Sequence, Iterable etc

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Adding type annotations to a function using TypeVar
+"""
+from typing import TypeVar, Optional, Any, Mapping, Union
+
+
+T = TypeVar("T")
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """
+    Adding type annotations to a function using TypeVar
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
