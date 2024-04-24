5fd9ba4d2f1138ace66c9301280e31b490ccd1bb
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-24 14:18:57 +0100

    Corrected file annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,17 +6,18 @@ max_delay
 """
 import asyncio
 import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     An async routine that takes in 2 int arguments: n and max_delay.
     wait_random imported, will be spawned n times with the specified
     max_delay
     """
-    i = random.uniform(0, max_delay)
-    results = []
+    i: float = random.uniform(0, max_delay)
+    results: List[float] = []
     for _ in range(n):
         results.append(await wait_random(max_delay))
     return (sorted(results))
