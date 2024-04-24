03a10970c0997726e5a18eeeb354cde186669813
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-24 21:52:48 +0100

    A coroutine that loop 10 times each time asynchronously wait 1 second

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A coroutine that takes no arguments. It would loop 10 times,
+each time asynchronously wait 1 second, then yield a random
+number between 0 and 10 using the random module
+"""
+import asyncio
+from random import uniform
+
+
+async def async_generator():
+    """
+    A coroutine that takes no arguments. It would loop 10 times,
+    each time asynchronously wait 1 second, then yield a random
+    number between 0 and 10 using the random module
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
