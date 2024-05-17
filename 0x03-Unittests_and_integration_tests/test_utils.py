88ac756768a62a95b5d812eeb9147673fe6a442a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-05-17 13:26:28 +0100

    Using context manager to createa a testcase for the access_nested_map method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,9 +17,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
-        """Testing the access_nested_map method"""
+        """ Testing the access_nested_map method """
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Testing KeyError is raised for the inputs """
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":
